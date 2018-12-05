b9129abdb5f736da7bbb52563bec68ac1b10aa99
Claudio <Claudio@Claudios-MacBook-Air.local>
2018-12-05 13:25:57 +0100

    @media query sorted

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const button = document.getElementById('btn');
 const resultHead = document.getElementById('exch-result-head');
 const resultPara = document.getElementById('exch-result-p');
 
-
-
 button.addEventListener('click', () => {
 	const input = document.getElementById('amount').value;
 	const currencyInput = document.getElementById('currencyFrom').value;
@@ -11,6 +9,7 @@ button.addEventListener('click', () => {
 	const euroNokRate = 9.53;
 	const dollarNokRate = 0.119;
 	const poundNokRate = 0.092;
+	
 	
 	if(currencyInput === 'nok' && currencyOutput === 'euro') {
 		resultHead.innerHTML = 'Euros: ' + (input / euroNokRate).toFixed(2); 
